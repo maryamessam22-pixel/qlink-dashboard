a32438f62260b907d63a15bcf692821f5d546dc1
maryamessam22-pixel <maryamessam22@gmail.com>
2026-04-06 00:50:58 +0200

    refactor: move router configuration directly into App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Layouts
+import MainLayout from './layouts/MainLayout';
+
+// Auth
+import AdminLogin from './features/auth/AdminLogin';
+
+// Web Dashboard
+import WebOverview from './features/web-dashboard/overview/WebOverview';
+
+// App Dashboard
+import AppOverview from './features/app-dashboard/overview/AppOverview';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Route for Login */}
+        <Route path="/login" element={<AdminLogin />} />
+
+        {/* Main Layout containing the Sidebar and Topbar */}
+        <Route path="/" element={<MainLayout />}>
+          
+          {/* Redirect root (/) to Web Overview by default */}
+          <Route index element={<Navigate to="/web/overview" replace />} />
+          
+          {/* Web Dashboard Routes */}
+          <Route path="web">
+            <Route path="overview" element={<WebOverview />} />
+            {/* We will add other web routes (CMS, Products, etc.) here later */}
+          </Route>
+
+          {/* App Dashboard Routes */}
+          <Route path="app">
+            <Route path="overview" element={<AppOverview />} />
+            {/* We will add other app routes (Users, Bracelets, etc.) here later */}
+          </Route>
+
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
